eef33a38240f1539de82d28d8d81e5d7806668b4
Anzen Digital <alex.c.valera94@hotmail.com>
2019-07-08 16:31:23 -0500

    Last changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Quote({ phrase }) {
+    return (
+        <div className="frase">
+            <h1>{phrase.quote}</h1>
+            <p>- {phrase.author}</p>
+        </div>
+    );
+}
 
 function App() {
+    const [phrase, getPhrase] = useState({});
+
+    // Consult
+    const fetchData = async () => {
+        const result = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+        // Agregar el resultado de la API al State
+        getPhrase(result.data[0]);
+    };
+
+    // Consult a Rest-API
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
-        <div>
-            <p>Hello</p>
+        <div className="contenedor">
+            <Quote phrase={phrase} />
+            <button onClick={fetchData}>Generar Nueva</button>
         </div>
     );
 }
